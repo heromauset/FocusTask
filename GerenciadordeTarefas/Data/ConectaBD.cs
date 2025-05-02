f40fd7434f9417e53f29623690e24d0e10855500
heromauset <116310855+heromauset@users.noreply.github.com>
2025-05-02 09:57:03 -0300

    Update ConectaBD.cs

--- GerenciadordeTarefas/Data/ConectaBD.cs
+++ GerenciadordeTarefas/Data/ConectaBD.cs
@@ -4,7 +4,7 @@ namespace GerenciadordeTarefas.Data
 {
     internal class ConectaBD
     {
-        string ConnctionString = @"Server=tcp:focustasks.database.windows.net,1433;Initial Catalog=gerenciadordetarefasbd;Persist Security Info=False;User ID=adm;Password=Dinossaurorex1@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        string ConnctionString = @"Server=tcp:focustasks.database.windows.net,1433;Initial Catalog=gerenciadordetarefasbd;Persist Security Info=False;User ID=adm;Password=password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public SqlConnection SqlConn()
         {
             {
